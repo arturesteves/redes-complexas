6aea65fa03b9597d80ed05f1719abc2806481930
arturesteves <arturmesteves@hotmail.com>
2017-10-31 06:24:52 +0000

    Code to generate metric for the 3 metrics, generate more metrics on the file, generate .gif file at the end of each attack. Images, .gif and logs of each network attack

--- Redes/Network Analysis Scripts/main.py
+++ Redes/Network Analysis Scripts/main.py
@@ -1,21 +1,6 @@
 '''
     This project is based on python 3.6.1
 
-
-    TODOS:
-        Generate metrics:
-            Average Degree - x
-            Degree Distribution - x
-            Average Path Length - x
-            Eigenvector Centrality
-
-
-        Print metrics in file - x
-        Print metrics in a file for every iteration - x
-        Save the plots for every iteration (save as pdf file, at least for now) - x
-        Plot average path length only after the iteration process is completed
-
-
     Docs support:
         https://www.cl.cam.ac.uk/teaching/1314/L109/tutorial.pdf
         https://www.cl.cam.ac.uk/~cm542/teaching/2010/stna-pdfs/stna-lecture8.pdf
@@ -28,9 +13,12 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import operator
-import sys
 import csv
+import imageio
+import random
 
+
+# import numpy as np
 
 # load a graph using the name of the network in the current folder
 def load_graph(network_name="diseasome"):
@@ -56,80 +44,127 @@ def load_graph(network_name="diseasome"):
     return G
 
 
-# orders the nodes using the metric
-def get_ordered_nodes(graph):
-    # For highest degree_centrality
-    # metric_dic = nx.degree_centrality(G)
+# orders nodes using one metric
+def sort_nodes_by_metric(graph, metric):
+    nodes = None
+    sorted_nodes = []
 
-    # For highest betweenness_centrality
-    metric_dic = nx.betweenness_centrality(graph)
-    sorted_nodes = sorted(metric_dic.items(), key=operator.itemgetter(1), reverse=True)
+    # switch by metric
+    if metric == "degree_centrality":
+        nodes = nx.degree_centrality(graph)
+        sorted_nodes = sorted(nodes.items(), key=operator.itemgetter(1), reverse=True)
+    elif metric == "betweenness_centrality":
+        nodes = nx.betweenness_centrality(graph)
+        sorted_nodes = sorted(nodes.items(), key=operator.itemgetter(1), reverse=True)
+    elif metric == "random":
+        nodes = nx.betweenness_centrality(graph)
+        sorted_nodes_aux = nodes
+        values = list(sorted_nodes_aux.values())
+        random.shuffle(values)
+        sorted_nodes_aux = dict(zip(sorted_nodes_aux.keys(), values))
+
+        for key, value in sorted_nodes_aux.items():
+            sorted_nodes.append((key, value))
+
     return sorted_nodes
 
 
-# todo: fazer com que esta funcao aceite a metrica e a ordem com que os nos devem ser removidos
-# assim da para fazer uma comparacao entre metricas
 # plot graph after iterations
-def print_iterations(network_name, graph, iterations, jump, strategy=None):
-    print("Init print iterations...")
-    # todo: receber a estrategia de attack, aqui é pela betwenneess e a outra por agora sera a random; acrescentar a random aqui
-    sorted_nodes = get_ordered_nodes(graph)
-    print("Graph sorted by betweenness")
-    print("Starting iterations...")
-    figure = plt.figure()
-    figure.canvas.set_window_title(network_name)
-    for i in range(0, iterations):
+def attack_network(network_name, graph, iterations, jump, metric="degree_centrality"):
+    print("Attack " + network + " by " + metric + "...")
+    # nodes sorted by highest betweenness centrality
+    sorted_nodes = sort_nodes_by_metric(graph, metric)
+    print("Graph sorted by " + metric)
+    original_number_nodes = len(sorted_nodes)
+    n, e, ad, nnr, rnp, ncc, lcc, apl, aplacc, dlcc, dc, bc = calculate_metrics(graph, 0, original_number_nodes)
+
+    # plot original graph
+    plot_graph(graph=graph, title="Network: " + network_name + " Original",
+               file_name=network_name + " - " + metric + " - Original", extension="png",
+               x_axis=original_number_nodes, metric=metric, dc=dc, bc=bc, r=sorted_nodes)
+
+    #  write metrics
+    write_iteration_metrics(num_nodes=n, num_edges=e, average_degree=ad,
+                            number_nodes_removed=nnr,
+                            removed_nodes_percentage=rnp,
+                            number_connected_components=ncc,
+                            max_connected_component_size=lcc,
+                            average_path_length=apl,
+                            average_path_length_all_cc=aplacc,
+                            network_name=network_name,
+                            diameter_largest_connected_component=dlcc,
+                            degree_centrality=dc,
+                            betweenness_centrality=bc,
+                            iteration=0)
+
+    for i in range(1, iterations):
         node_to_remove = sorted_nodes[i][0]
         print("Removing node: %s", node_to_remove)
-        # graph.remove_node(node_to_remove)
         graph.remove_node(node_to_remove)
-        # to skip ploting all iterations, zero and multiples of jump
+
+        # to skip ploting all iterations
         if i % jump == 0:
-            nx.draw(graph, pos=nx.spring_layout(graph), node_size=5, node_color='orange')
-
+            print("save iteration: " + str(i))
             #  calculate metrics
-            nodes = len(nx.nodes(graph))
-            edges = len(nx.edges(graph))
-            average_degree = compute_average_degree(graph)
-            number_nodes_removed = jump
-            removed_nodes_percentage = ((nodes - jump) * 100) / nodes
-            connected_components = nx.connected_components(graph)
-            number_connected_components = nx.number_connected_components(graph)
-            max_connected_component_size = max(connected_components,
-                                               key=len)  # todo: alterar isto, para retornar o número e nao uma lista gigante
+            n, e, ad, nnr, rnp, ncc, lcc, apl, aplacc, dlcc, dc, bc = calculate_metrics(graph, i, original_number_nodes)
+            #  write metrics
+            plot_graph(graph=graph, title="Network: " + network_name + " - Iteration " + str(i),
+                       file_name=network_name + " - " + metric + " - Iteration " + str(i), extension="png",
+                       x_axis=n, metric=metric, dc=dc, bc=bc, r=sorted_nodes)
 
-            # fetch the greater connected component, because of the apl
-            larger_connected_component = max(nx.connected_component_subgraphs(graph), key=len)
-            average_path_length = nx.average_shortest_path_length(larger_connected_component)
-
-            # about average path length: try floyd_warshall and astar_path (explicar relatorio se valer a pena)
-
-            nodes_centrality = nx.eigenvector_centrality(graph)
-
-            # EigenVector Centrality
-            # ver mas métricas que estão no pdf
-
-            #  end metrics
-            write_iteration_metrics(num_nodes=nodes, num_edges=edges, average_degree=average_degree,
-                                    number_nodes_removed=number_nodes_removed,
-                                    removed_nodes_percentage=removed_nodes_percentage,
-                                    number_connected_components=number_connected_components,
-                                    max_connected_component_size=max_connected_component_size,
-                                    average_path_length=average_path_length,
-                                    network_name=network_name, iteration=i)
+            write_iteration_metrics(num_nodes=n, num_edges=e, average_degree=ad,
+                                    number_nodes_removed=nnr,
+                                    removed_nodes_percentage=rnp,
+                                    number_connected_components=ncc,
+                                    max_connected_component_size=lcc,
+                                    average_path_length=apl,
+                                    average_path_length_all_cc=aplacc,
+                                    network_name=network_name,
+                                    diameter_largest_connected_component=dlcc,
+                                    degree_centrality=dc,
+                                    betweenness_centrality=bc,
+                                    iteration=i)
 
             print("Number of the iteration: " + str(i))
-            plt.title("Network: " + network_name + " - Iteration " + str(i))
-            plt.suptitle("Network: " + network_name + " - Iteration " + str(i))
-            # plt.annotate('Connected Components: ' + str(number_connected_components), xy=(0.1, 0.1), xytext=(0.112, 0.15))
-            plt.draw()
-            plt.savefig("networks/output/Network " + network_name + " - Iteration " + str(i) + ".pdf")
-            # plt.show()
-            plt.close()
-            plt.gcf().clear()  # forget old plot
 
-    pass
-    # here plot the average path length plot, outside the for loop
+    print("End of attack")
+
+
+def calculate_metrics(graph, iteration, original_number_nodes):
+    print("calculate metrics...")
+    nodes = len(nx.nodes(graph))
+    edges = len(nx.edges(graph))
+    average_degree = compute_average_degree(graph)
+    number_nodes_removed = iteration
+    removed_nodes_percentage = (number_nodes_removed * 100) / original_number_nodes
+    connected_components = nx.connected_components(graph)
+    number_connected_components = nx.number_connected_components(graph)
+    max_connected_component_size = max(connected_components,
+                                       key=len)
+
+    # fetch the greater connected component, because of the apl
+    larger_connected_subgraph = max(nx.connected_component_subgraphs(graph), key=len)
+    larget_connected_graph_nodes = len(larger_connected_subgraph.nodes)
+    larger_connected_component = max(larger_connected_subgraph)
+    average_path_length_largest_connected_component = nx.average_shortest_path_length(larger_connected_subgraph)
+    diameter_largest_connected_component = nx.diameter(larger_connected_subgraph)
+    # average_path_length = nx.average_shortest_path_length(graph)
+
+    connected_components_graphs = nx.connected_component_subgraphs(graph)
+    average_path_length_all_connected_components = []
+    for component in connected_components_graphs:
+        average_path_length_all_connected_components.append(nx.average_shortest_path_length(component))
+
+    # eigenvector_centrality = nx.eigenvector_centrality(graph)
+    degree_centrality = nx.degree_centrality(graph)
+    betweenness_centrality = nx.betweenness_centrality(graph)
+
+    print("calculate metrics...")
+    return (nodes, edges, average_degree, number_nodes_removed, removed_nodes_percentage,
+            number_connected_components,
+            larger_connected_component, average_path_length_largest_connected_component,
+            average_path_length_all_connected_components, diameter_largest_connected_component,
+            degree_centrality, betweenness_centrality)
 
 
 def compute_average_degree(graph):
@@ -137,29 +172,39 @@ def compute_average_degree(graph):
     return (2 * len(graph.edges)) / len(graph.nodes)
 
 
-def generic_plot(**kwargs):
-    plt.figure()
-    plt.grid(kwargs.pop("grid", False))
-    plt.plot(kwargs.pop("x_axis", []), kwargs.pop("y_axis", []), kwargs.pop("plot_style", "b-"))
-    plt.yscale(kwargs.pop("y_scale", "linear"))
-    plt.xscale(kwargs.pop("x_scale", "linear"))
-    plt.xlabel(kwargs.pop("x_label", ""))
-    plt.ylabel(kwargs.pop("y_label", ""))
-    plt.title(kwargs.pop("title", ""))
-    plt.savefig("networks/output/" + kwargs.pop("file_name") + ".pdf")
-    plt.close()
-    plt.gcf().clear()  # forget old plot
+def plot_graph(**kwargs):
+    print("Init plotting...")
+    graph = kwargs.pop("graph")
+    if graph is not None:
+        nodes_sorted = None
+        factor = None
+        metric = kwargs.pop("metric")
+        if metric == "degree_centrality":
+            factor = 1000
+            nodes_sorted = kwargs.pop("dc")
+        elif metric == "betweenness_centrality":
+            factor = 500
+            nodes_sorted = kwargs.pop("bc")
+        elif metric == "random":
+            factor = 500
+            nodes_sorted = dict(kwargs.pop("r"))
 
+        plt.figure()
+        nx.draw(graph, pos=nx.spring_layout(graph), node_size=[v * factor for v in nodes_sorted.values()],
+                node_color='orange')
+        plt.grid(kwargs.pop("grid", False))
+        # plt.plot(kwargs.pop("x_axis", []), kwargs.pop("y_axis", []), kwargs.pop("plot_style", "b-"))
+        plt.yscale(kwargs.pop("y_scale", "linear"))
+        plt.xscale(kwargs.pop("x_scale", "linear"))
+        plt.xlabel(kwargs.pop("x_label", ""))
+        plt.ylabel(kwargs.pop("y_label", ""))
+        plt.title(kwargs.pop("title", ""))
+        plt.savefig("networks/output/" + kwargs.pop("file_name") + "." + kwargs.pop("extension", "png"))
+        # plt.show()
+        plt.close()
+        plt.gcf().clear()  # forget old plot
 
-# plot number of nodes x average length of paths
-# faz sentido quando se estiverem a retirar os nós!
-# nodes: number of nodes; average_path_length: list of values of the paths according with
-# the nodes number, this list should be order from when there is fewer nodes to the a full node scene
-# plot example: https://www.researchgate.net/figure/270222207_fig13_Comparisons-of-average-length-for-all-paths
-def plot_average_path_length_vs_nodes(graph, nodes, average_path_length, ):
-    # todo: call with proper parameters
-    generic_plot()
-    pass
+    print("End plotting...")
 
 
 # currently plotting
@@ -184,37 +229,82 @@ def degree_distribution(graph, network_name):
 
 def write_iteration_metrics(**kwargs):
     # graph, network_name, iteration):
-    with open("networks/output/" + kwargs.pop("network_name") + "_attack_metrics.txt", 'a+') as file:
+    print("Writing to file...")
+    with open("networks/output/" + kwargs.pop("network_name") + " - " + metric + " - attack.txt", 'a+') as file:
         file.write("Iteration number: {0}\n".format(kwargs.pop("iteration")))
         file.write("Number of nodes: {0}\n".format(kwargs.pop("num_nodes")))
         file.write("Number of edges: {0}\n".format(kwargs.pop("num_edges")))
         file.write("Average degree: {0}\n".format(kwargs.pop("average_degree")))
         file.write("Number of nodes removed: {0}\n".format(kwargs.pop("number_nodes_removed")))
-        file.write("Percentage of nodes removed: {0}\n".format(kwargs.pop("removed_nodes_percentage")))
+        file.write("Percentage of nodes removed: {0}%\n".format(kwargs.pop("removed_nodes_percentage")))
         file.write("Number of connected components: {0}\n".format(kwargs.pop("number_connected_components")))
-        file.write("Size of the biggest connected component: {0}\n".format(kwargs.pop("max_connected_component_size")))
-        file.write("Average path length: {0}\n".format(kwargs.pop("average_path_length")))
+        file.write(
+            "Size of the biggest connected component: {0} nodes\n".format(kwargs.pop("max_connected_component_size")))
+        file.write(
+            "Average path length of largest connected component: {0}\n".format(kwargs.pop("average_path_length")))
+        file.write(
+            "Average path length of all connected components: {0}\n".format(kwargs.pop("average_path_length_all_cc")))
+        file.write(
+            "Diameter largest connected component: {0}\n".format(kwargs.pop("diameter_largest_connected_component")))
+        # file.write("Eigenvector centrality: {0}\n".format(kwargs.pop("eigenvector_centrality")))
+        file.write("Degree centrality: {0}\n".format(kwargs.pop("degree_centrality")))
+        file.write("Betweenness centrality: {0}\n".format(kwargs.pop("betweenness_centrality")))
         file.write("--------------------------------------------------\n")
+
+    print("End writing to file.")
+
+
+def generate_gif(network, iterations, jump, metric):
+    base = "networks/output/" + network
+    filenames = [base + " - " + metric + " - Original.png"]
+    for i in range(1, iterations):
+        if i % jump == 0:
+            filenames.append(base + " - " + metric + " - Iteration " + str(i) + ".png")
+
+    images = []
+    for filename in filenames:
+        images.append(imageio.imread(filename))
+
+    imageio.mimsave(base + " - " + metric + " - attack.gif", images, duration=0.5)
+    print("\n " + base + " - " + metric + " - attack.gif, has been generated\n\n")
 
 
 if __name__ == "__main__":
-    # runs scripts
+    # running this script will take several minutes
     print("Network analysis")
-    biological_network = 'diseasome'
-    power_network = 'power'
     iterations = 101
-    graph = load_graph(network_name=biological_network)
-    '''
-     run 30 iterations on graph G to remove critical nodes
-     based on the metric betweenness centrality and plot the
-     graph every 5 iterations
-    '''
-    #print_iterations(biological_network, graph, iterations, 20)
+    jump = 20
 
+    """
+    network = "diseasome"
+    metric = "degree_centrality"
+    graph = load_graph(network_name=network)
+    attack_network(network, graph, iterations, jump, metric)
+    generate_gif(network, iterations, jump, metric)
 
+    metric = "betweenness_centrality"
+    graph = load_graph(network_name=network)
+    attack_network(network, graph, iterations, jump, metric)
+    generate_gif(network, iterations, jump, metric)
 
-    graph = load_graph(network_name=power_network)
-    #print_iterations(power_network, graph, iterations, 20)
+    metric = "random"
+    graph = load_graph(network_name=network)
+    attack_network(network, graph, iterations, jump, metric)
+    generate_gif(network, iterations, jump, metric)
 
-    # compute_average_degree(graph)
-    degree_distribution(graph, network_name=power_network)
+    """
+    network = "power"
+    metric = "degree_centrality"
+    graph = load_graph(network_name=network)
+    attack_network(network, graph, iterations, jump, metric)
+    generate_gif(network, iterations, jump, metric)
+
+    metric = "betweenness_centrality"
+    graph = load_graph(network_name=network)
+    attack_network(network, graph, iterations, jump, metric)
+    generate_gif(network, iterations, jump, metric)
+
+    metric = "random"
+    graph = load_graph(network_name=network)
+    attack_network(network, graph, iterations, jump, metric)
+    generate_gif(network, iterations, jump, metric)
