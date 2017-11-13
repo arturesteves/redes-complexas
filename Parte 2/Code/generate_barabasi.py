151c2f2d4242aa2324f30feee96c59bf75668918
moraispgsi <moraispgsi@gmail.com>
2017-11-13 19:03:16 +0000

    Optimization

--- Parte 2/Code/generate_barabasi.py
+++ Parte 2/Code/generate_barabasi.py
@@ -9,10 +9,26 @@ def seconds_to_time(seconds):
 
 start = time.time()
 
+# compute the number of shortest paths that pass through each node. Returns dict
+def num_spaths(graph):
+	start = time.time()
+	n_spaths = dict.fromkeys(graph, 0.0)
+	spaths = dict(nx.all_pairs_shortest_path(graph))
+
+	for source in spaths:
+		for path in spaths[source].items():
+			for node in path[1][1:]:  # ignore first element (source == node)
+				n_spaths[node] += 1  # this path passes through `node`
+
+	end = time.time()
+	elapsed_time = end - start
+	print("It took " + seconds_to_time(elapsed_time) + " to compute the number of shortest paths that pass through each node.")
+	return n_spaths, spaths
+
 n = 5000  # number of nodes
 m = 2  # number of edges for the preferential attachment
 graph = nx.barabasi_albert_graph(n, m)  # make a random scale-free graph using barabasi_albert model
-nx.write_gpickle(graph, './graph_scalefree_5000.txt')
+nx.write_gpickle(graph, './graph_scalefree_5000.p')
 
 end = time.time()
 elapsed_time = end - start
